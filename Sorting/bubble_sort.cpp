07d40be03d683d3c0f47d634b97544807b033f53
bedwal-deepak <deepakbedwalb5@gmail.com>
2022-04-01 14:47:53 +0530

    bubble sort

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,36 @@
+// TC: O(n^2) and best case TC: O(n) if array is sorted
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    // if array is sorted then we have to maitain a flag and break the loop
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
